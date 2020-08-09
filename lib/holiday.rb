2ef1387521c02ca1b2926165f68da6781eff913c
hersson ceballos <herssonceballos@gmail.com>
2020-08-09 15:22:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,9 @@ end
 
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash.each do |
+  holiday_hash.each do |season, holidays_name|
+    if season  == :winter 
+      holidays_name.each do |
 
   
   
